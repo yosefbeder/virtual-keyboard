f6771358baf3ee07e7fa593e65f6c31e7ba15011
yosefbeder <dryosefbeder@gmail.com>
2021-08-09 19:15:46 +0200

    Updates

---
+++ precache-manifest.d3eba5a93142a6f31ec68b2875b46806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4181818c50b4d01361c4f57639019e",
+    "url": "/@yosefbeder/virtual-keyboard/index.html"
+  },
+  {
+    "revision": "2f762b197efa65e0b2de",
+    "url": "/@yosefbeder/virtual-keyboard/static/css/main.8d26eae5.chunk.css"
+  },
+  {
+    "revision": "7bda7ceca3bb1d9f2873",
+    "url": "/@yosefbeder/virtual-keyboard/static/js/2.59b8ae52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/@yosefbeder/virtual-keyboard/static/js/2.59b8ae52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f762b197efa65e0b2de",
+    "url": "/@yosefbeder/virtual-keyboard/static/js/main.fb4ab44c.chunk.js"
+  },
+  {
+    "revision": "68a825160ef189643da4",
+    "url": "/@yosefbeder/virtual-keyboard/static/js/runtime-main.231d35d1.js"
+  }
+]);
